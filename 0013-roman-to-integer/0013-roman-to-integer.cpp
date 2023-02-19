0409c9fee615a1940538a3a90ded87696e6204dc
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-02-19 11:41:47 +0100

    Time: 11 ms (71.11%), Space: 7.9 MB (50.41%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -5,11 +5,11 @@ class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char,int> myMap = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-        char latest = '\0';
-        int sum = 0;
+        char latest = myMap.find(s[(s.size() - 1)])->first;
+        int sum = myMap.find(s[(s.size() - 1)])->second;
         
-        for (int current = (s.size() - 1); current >= 0; current--)  {
-            if ((latest == '\0') || (myMap.find(s[current])->second >= myMap.find(latest)->second))  {
+        for (int current = (s.size() - 2); current >= 0; current--)  {
+            if (myMap.find(s[current])->second >= myMap.find(latest)->second)  {
                 sum += myMap[s[current]];
             }  else  {
                 sum -= myMap[s[current]];
