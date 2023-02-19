8b1de562104b0d3f402044655027b1060b9c825a
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-02-19 12:17:23 +0100

    Time: 12 ms (78.11%), Space: 6.2 MB (11.06%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:    
-    bool isPalindrome(int x) {
-        string s = to_string(x);
-        reverse(s.begin(), s.end());
-        
-        if (to_string(x) == s)  {
-            return true;
+    int digitsCalc(int x) {
+        int i = 0;
+        while(x > 0) {
+            x /= 10;
+            i++;
         }
-        return false;
-        
-        
-        /*
-        string s = to_string(x);
-        int i = 0; int j = s.size() - 1;
-        
-        while (i < j)  {
-            if (s[i] != s[j])  {
+
+        return i;
+    }
+    
+    bool isPalindrome(int x) {
+        if(x < 0)
                 return false;
-            } 
-            i++; j--;
-        }  
-        
-        return true;
-        */
+
+            if(x < 10)
+                return true;
+
+            int digits = digitsCalc(x);
+
+            int i = 1;
+
+            int divider1 = (int) pow(10.0, (double) (digits - i));
+            int divider2 = 1;
+
+            while (i <= digits / 2) {
+                if((x / divider1) % 10 != (x % (divider2 * 10)) / divider2)
+                    return false;
+
+                divider1 /= 10;
+                divider2 *= 10;
+
+                i++;
+            }
+
+            return true;
     }
 };
-
-
-// FOLLOW UP
-    /*
-    bool isPalindrome(int x) {
-        vector<int> number;  // store digits
-        
-        if (x < 0)  {return false;}
-        if (x == 0)  {return true;}
-        
-        while (x != 0)  {
-            number.push_back((x % 10));
-            x /= 10;
-        }
-        
-        for (int current = 0; current < number.size(); current++)  {
-            if (number[current] != number[number.size() - current - 1])  {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    */
