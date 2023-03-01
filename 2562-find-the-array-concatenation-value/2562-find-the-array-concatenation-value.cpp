a07b3a16f24ea95352fad2e7b8d41703d513f7a5
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-03-01 16:57:26 +0100

    Time: 6 ms (83.41%), Space: 9 MB (79.44%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.cpp
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.cpp
@@ -4,6 +4,25 @@ class Solution {
 public:
     long long findTheArrayConcVal(vector<int>& nums) {
         long long sum = 0;
+        int i = 0; int j = nums.size() - 1;
+        
+        while (i < j)  {
+            sum += stoll((to_string(nums[i]) + to_string(nums[j])));
+            i++; j--;
+        }
+        
+        if (i == j)  {
+            return (sum + nums[i]);
+        }
+        
+        return sum;
+    }
+};
+
+/*
+// erasing begin is O(n), combined with to_string()? bad bad
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long sum = 0;
         
         while (!nums.empty())  {
             if (nums.size() == 1)  {
@@ -15,4 +34,4 @@ public:
         
         return sum;
     }
-};
+*/
