97a753fbd9f3209078e78dbe9f5f6764fcbfb1f3
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-03-01 11:00:26 +0100

    Time: 14 ms (24.08%), Space: 9.1 MB (79.44%) - LeetHub

---
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long sum = 0;
+        
+        while (!nums.empty())  {
+            if (nums.size() == 1)  {
+                return (sum + nums.front());
+            }
+            sum += stoll(to_string(nums.front()) + to_string(nums.back()));
+            nums.erase(nums.begin()); nums.pop_back();
+        }
+        
+        return sum;
+    }
+};
