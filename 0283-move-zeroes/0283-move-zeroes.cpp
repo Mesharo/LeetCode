6bfd0b0157fcc440675abbafc639c4296cc4d8a6
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-03-02 09:43:22 +0100

    Time: 27 ms (55.10%), Space: 19.1 MB (98.93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
+    void mySwap(vector<int>& nums, int i, int j)  {
+        int whatever = nums[i];
+        nums[i] = nums[j];
+        nums[j] = whatever;
+    }
+    
     void moveZeroes(vector<int>& nums) {
         int index = 0;
         for (int current = 0; current < nums.size(); current++)  {
             if (nums[current] != 0)  {
-                swap(nums[index], nums[current]);
+                mySwap(nums, index, current);
                 index++;
             }
         }
     }
 };
+
