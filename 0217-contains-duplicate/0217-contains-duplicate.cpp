3c3029c84af0cc42925faf5c491b554149eda2d9
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-03-02 11:33:40 +0100

    Time: 146 ms (50.39%), Space: 69.6 MB (37.59%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -5,11 +5,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         unordered_map<int, int> myMap;
         for (int current = 0; current < nums.size(); current++)  {
-            if (myMap.find(nums[current]) == myMap.end())  {
-                myMap[nums[current]] = 1;
-                continue;
+            if (myMap.find(nums[current]) != myMap.end())  {
+                return true;
             }
-            return true;
+            myMap[nums[current]] = 1;
         }
         
         return false;
