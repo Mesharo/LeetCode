5255a33d961bf8c474f78d28c3b6078a95f9e90c
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-03-08 11:20:06 +0100

    Time: 5 ms (30.98%), Space: 7.2 MB (42.54%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        for (int current = 0; current < matrix.size(); current++)  {
+            reverse(matrix[current].begin(), matrix[current].end());
+        }
+        
+        for (int i = 0; i < matrix.size(); i++)  {
+            for (int j = 0; j < matrix.size() - i; j++)  {  
+                if ((i + j) == (matrix.size() - 1))  {
+                    continue;
+                }
+                
+                swap(matrix[i][j], matrix[matrix.size() - 1 - j][matrix.size() - 1 - i]);              
+            }
+        }
+    }
+};
