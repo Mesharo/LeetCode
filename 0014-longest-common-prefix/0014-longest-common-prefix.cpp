5cdac300173e1485a68a983f87c670df94071e3c
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-03-08 12:27:11 +0100

    Time: 5 ms (45.55%), Space: 9.4 MB (21.05%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix = strs[0];
+        for (int current = 1; current < strs.size(); current++)  {
+            string next = "";
+            for (int i = 0; i < prefix.length(); i++)  {
+                if (prefix[i] == strs[current][i])  {
+                    next += prefix[i];
+                } else  {
+                    break;
+                }
+            }
+            if (next == "")  {
+                return next;
+            }
+            prefix = next;
+        }
+        return prefix;
+    }
+};
