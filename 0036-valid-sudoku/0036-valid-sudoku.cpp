51025488d7962007cb97869065f8e72e6f6385cb
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-03-12 17:25:44 +0100

    Time: 40 ms (11.38%), Space: 19.5 MB (44.24%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,33 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_map<char, vector<vector<int>>> myMap;
+    
+        for (int x = 0; x < 9; x++)  {
+            for (int y = 0; y < 9; y++)  {
+                
+                if (board[x][y] == '.')  {
+                    continue;
+                }
+                
+                int subbox = (x / 3) * 3 + (y / 3);
+                cout << subbox << " ";
+                
+                if (myMap.find(board[x][y]) == myMap.end())  {
+                    myMap[board[x][y]] = {{x, y, subbox}};
+                } else  {
+                    for (auto current : myMap[board[x][y]])  {
+                        if ((current[0] == x) || (current[1] == y) || (current[2] == subbox))  {
+                            return false;
+                        }
+                    }
+                    myMap[board[x][y]].push_back({x, y, subbox});
+                }
+            }
+        }
+        
+        return true;
+    }
+};
