de7f7692c9629cde5f04e3084ac0a84720b891c5
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-04-03 16:57:06 +0200

    Time: 3 ms (75.17%), Space: 8 MB (27.61%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        unordered_map<int,int> myMap;
+        vector<int> notIn;
+        
+        for (auto i : arr1)  {
+            if (myMap.find(i) != myMap.end())  {
+                myMap[i]++;
+            } else  {
+                if (find(arr2.begin(), arr2.end(), i) == arr2.end())  {
+                    notIn.push_back(i);
+                }
+                myMap[i] = 1;
+            }
+        }
+        
+        sort(notIn.begin(), notIn.end());
+        arr2.insert(arr2.end(), notIn.begin(), notIn.end());
+        
+        int index = 0;
+        for (auto i : arr2)  {
+            while (myMap[i] != 0)  {
+                arr1[index] = i;
+                myMap[i]--; index++;
+            }
+        }
+                
+        return arr1;
+    }
+};
