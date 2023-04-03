0143b5542b53ebf227a96c4e673a8fdc2513990b
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-04-03 16:23:08 +0200

    Time: 0 ms (100.00%), Space: 8.3 MB (74.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int count[3] = {0, 0, 0};
-        for (auto i : nums)  {
-            count[i]++;
-        }
         
-        auto index = 0;
-        for (auto i = 0; i < 3; i++)  {
-            while (count[i] != 0)  {
-                nums[index] = i;
-                count[i]--; index++;
+        // Dutch national flag problem
+        int red = 0; int white = 0; int blue = nums.size() - 1;
+
+        while (white <= blue)  {
+            if (nums[white] == 0)  {
+                swap(nums[red], nums[white]);
+                red++; white++;
+            } else if (nums[white] == 1)  {
+                white++;
+            } else  {
+                swap(nums[white], nums[blue]);
+                blue--;
             }
         }
     }
 };
+
+
+/*
+My simple solution:
+    int count[3] = {0, 0, 0};
+    for (auto i : nums)  {
+        count[i]++;
+    }
+        
+    auto index = 0;
+    for (auto i = 0; i < 3; i++)  {
+        while (count[i] != 0)  {
+            nums[index] = i;
+            count[i]--; index++;
+        }
+    }
+*/
