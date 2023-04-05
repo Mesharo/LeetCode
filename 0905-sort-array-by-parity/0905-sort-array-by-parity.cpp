7ab5059c70aa1e5b026b71074c1d74f20fd9d4dd
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-04-05 20:56:47 +0200

    Time: 6 ms (91.58%), Space: 16.2 MB (67.45%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -5,12 +5,12 @@ public:
         int first = 0, last = nums.size() - 1;   
         while (first < last)  {
             // If first is even, skip
-            if (nums[first] % 2 == 0)  {
+            if (!(nums[first] & 1))  {
                 first++; continue;
             }
             
             // If last is even, swap with odd first
-            if (nums[last] % 2 == 0)  {
+            if (!(nums[last] & 1))  {
                 swap(nums[first], nums[last]);
                 first++;
             }
