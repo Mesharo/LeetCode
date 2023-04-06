b59217b9505ea70e45dcd0af12718d4281247547
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-04-06 12:21:52 +0200

    Time: 43 ms (73.30%), Space: 12.2 MB (42.72%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -13,18 +13,3 @@ public:
         return result;
     }
 };
-
-/*
-        int result = 0;
-        for (int column = 0; column < strs[0].length(); column++)  {
-            int first = strs[0][column];
-            for (int current = 1; current < strs.size(); current++)  {
-                if (strs[current][column] < first)  {
-                    result++;
-                    break;
-                }
-                first = strs[current][column];
-            }
-        }
-        return result;
-*/
