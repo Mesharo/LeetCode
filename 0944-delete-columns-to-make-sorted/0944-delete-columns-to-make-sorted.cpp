969ddb4ec94bdebf27226b1d3702ed8e5c7db246
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-04-06 12:08:52 +0200

    Time: 48 ms (38.22%), Space: 12.3 MB (42.72%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -4,9 +4,7 @@ public:
         int result = 0;
         for (int column = 0; column < strs[0].length(); column++)  {
             int first = strs[0][column];
-            //cout << "NEW COLUMN"<< endl;
             for (int current = 1; current < strs.size(); current++)  {
-                //cout << "FIRST: " << (char)first << " SECOND: " << strs[current][column] << endl;
                 if (strs[current][column] < first)  {
                     result++;
                     break;
