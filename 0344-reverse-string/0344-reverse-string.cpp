e550b3e451aef66f954b6f42d29d4bbe2071e507
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-04-08 12:39:51 +0200

    Time: 18 ms (86.38%), Space: 23.2 MB (80.42%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         for (auto current = 0; current < (s.size() / 2); current++)  {
-            swap (s[current], s[s.size() - 1 - current]);
+            swap(s[current], s[s.size() - 1 - current]);
         }
     }
 };
+
+//
