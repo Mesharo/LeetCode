19cd0ea312ffdc4ba4c76adac927470b9078737c
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-06-26 11:06:08 +0200

    Time: 72 ms (70.99%), Space: 20.9 MB (5.96%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,15 +1,22 @@
 class MinStack:
     def __init__(self):
         self.storage = []
+        self.min = 2147483648 # 2^31
 
     def push(self, val: int) -> None:
-        self.storage.append(val)
+        if not self.storage:
+            self.storage.append([val, val])
+            return
+        if self.storage[-1][1] > val:
+            self.storage.append([val, val])
+        else:
+            self.storage.append([val, self.storage[-1][1]])
 
     def pop(self) -> None:
         self.storage.pop()
 
     def top(self) -> int:
-        return self.storage[-1]
+        return self.storage[-1][0]
 
     def getMin(self) -> int:
-        return min(self.storage)
+        return self.storage[-1][1]
