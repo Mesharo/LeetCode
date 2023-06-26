aa64c6838ed333e058fa3a4c8be8d765007eca72
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-06-26 11:11:40 +0200

    Time: 83 ms (22.90%), Space: 20.8 MB (9.16%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -4,10 +4,7 @@ class MinStack:
         self.min = 2147483648 # 2^31
 
     def push(self, val: int) -> None:
-        if not self.storage:
-            self.storage.append([val, val])
-            return
-        if self.storage[-1][1] > val:
+        if not self.storage or self.storage[-1][1] > val:
             self.storage.append([val, val])
         else:
             self.storage.append([val, self.storage[-1][1]])
