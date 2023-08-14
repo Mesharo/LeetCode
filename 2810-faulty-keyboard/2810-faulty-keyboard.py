58faf12291086f3f55f580a369ca9e9cc5dabb9d
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-08-14 12:28:37 +0200

    Time: 53 ms (33.71%), Space: 16.3 MB (71.84%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.py
@@ -0,0 +1,14 @@
+class Solution:
+    def finalString(self, s: str) -> str:
+        res = ""
+        
+        for x in s:
+            if x == 'i':
+                res = self.reverseString(res)
+                continue
+            res += x
+            
+        return res
+
+    def reverseString(self, s: str) -> str:
+        return s[::-1]
