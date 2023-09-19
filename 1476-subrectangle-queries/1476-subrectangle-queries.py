bb50d8f59e99b881c1694470df744b5618d5c17f
Mesharo <79778818+Mesharo@users.noreply.github.com>
2023-09-19 13:48:36 +0200

    Time: 178 ms (22.94%), Space: 18.3 MB (38.53%) - LeetHub

--- 1476-subrectangle-queries/1476-subrectangle-queries.py
+++ 1476-subrectangle-queries/1476-subrectangle-queries.py
@@ -1,9 +1,12 @@
 class SubrectangleQueries:
     def __init__(self, rectangle: List[List[int]]):
         self.grid = rectangle  
-        self.count = 0
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
+        for x in range(row1, row2 + 1):
+            for y in range(col1, col2 + 1):
+                self.grid[x][y] = newValue
+        """
         while row1 <= row2:
             while col1 <= col2:
                 self.grid[row1][col1] = newValue
@@ -11,7 +14,8 @@ class SubrectangleQueries:
                 self.count += 1
             row1 += 1
             col1 -= self.count
-            self.count = 0                
+            self.count = 0  
+        """
     
     def getValue(self, row: int, col: int) -> int:
         return self.grid[row][col]
